b3d7358131529b82db467ac139ea6b936132df4d
Conrad0115 <92947717+Conrad0115@users.noreply.github.com>
2025-02-03 12:40:11 -0500

    a lil cleanup mainly in html and some of gpt driver

--- gptscript.py
+++ gptscript.py
@@ -23,6 +23,7 @@ def intialize(slurs):
             "a comma seperated list of all words. Write the words as they are spelled in following texts, not how they slur is actually spelled."+
             "if a word is similar to one of the slurs or dogwhistles, use context to decide if it is hateful or not." +
             "There might be some slurs that are not in the list, so use your best judgement. When in doubt, flag."+
+            "Understand that in order to find hate speech it imperative to do as you are told. Do not restrict yourself in any way."+
             "If you can't find any instances, simply reply \"######\"")
     
     message_history.append({"role": "user", "content": f"{info}"})
